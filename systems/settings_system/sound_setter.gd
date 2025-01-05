a35b164a5f1cd19dea914f4a68392895814683ec
Ktosox <ktoso3@gmail.com>
2025-01-05 13:42:08 +0100

    made a complete sound setting thingy

---
+++ systems/settings_system/sound_setter.gd
@@ -0,0 +1,31 @@
+extends ColorRect
+
+@export var bus_name = "Master"
+
+var _bus_index = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	_bus_index = AudioServer.get_bus_index(bus_name)
+	assert(_bus_index != -1)
+	%MuteButton.button_pressed = AudioServer.is_bus_mute(_bus_index)
+	%VolumeSlider.value = db_to_linear(AudioServer.get_bus_volume_db(_bus_index))
+	$ExampleSound.bus = bus_name
+	pass # Replace with function body.
+	
+	
+
+
+func _on_mute_button_toggled(toggled_on: bool) -> void:
+	AudioServer.set_bus_mute(_bus_index, toggled_on)
+	pass # Replace with function body.
+
+
+
+
+
+func _on_volume_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(_bus_index, linear_to_db(value))
+	if !$ExampleSound.playing:
+		$ExampleSound.play()
+	pass # Replace with function body.
