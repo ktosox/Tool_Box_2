74d490f45e33e5e1e29ca94cc857f08fd61de5fe
Ktosox <ktoso3@gmail.com>
2025-08-02 19:57:18 +0200

    started working on the first draft of the shader showcase

---
+++ projects/shader_showcase/screen_shaders.gd
@@ -0,0 +1,6 @@
+extends HBoxContainer
+
+# this thing handles:
+# - collecting a list of shaders and putting them on the ItemList
+# - accepting selections from ItemList and 
+# - applying selections to DemoImage
